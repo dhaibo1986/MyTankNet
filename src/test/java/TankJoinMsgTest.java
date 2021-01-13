d8bd24810a0fd28dc442b4cb5df319fdf7872023
haibo.duan <dhaibo1986@sohu.com>
2021-01-13 20:46:18 +0800

    测试用例，处理decoder和encoder

---
+++ src/test/java/TankJoinMsgTest.java
@@ -0,0 +1,66 @@
+import com.dhb.tank.coders.TankJoinMsgDecoder;
+import com.dhb.tank.coders.TankJoinMsgEncoder;
+import com.dhb.tank.comms.Dir;
+import com.dhb.tank.comms.Group;
+import com.dhb.tank.mode.TankJoinMsg;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.embedded.EmbeddedChannel;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+public class TankJoinMsgTest {
+
+	@Test
+	void testEncoder() {
+		EmbeddedChannel ch = new EmbeddedChannel();
+		UUID id = UUID.randomUUID();
+		TankJoinMsg msg = new TankJoinMsg(5,10, Dir.DOWN,true, Group.GOOD,id);
+		ch.pipeline()
+				.addLast(new TankJoinMsgEncoder());
+		ch.writeOutbound(msg);
+
+		ByteBuf buf = (ByteBuf) ch.readOutbound();
+
+		int x = buf.readInt();
+		int y = buf.readInt();
+		int dirOrdinal = buf.readInt();
+		Dir dir = Dir.values()[dirOrdinal];
+		boolean moving = buf.readBoolean();
+		int groupOrdinal = buf.readInt();
+		Group group = Group.values()[groupOrdinal];
+		UUID uuid = new UUID(buf.readLong(),buf.readLong());
+
+		Assertions.assertEquals(5,x);
+		Assertions.assertEquals(10,y);
+		Assertions.assertEquals(Dir.DOWN,dir);
+		Assertions.assertEquals(true,moving);
+		Assertions.assertEquals(Group.GOOD,group);
+		Assertions.assertEquals(id,uuid);
+	}
+
+	@Test
+	void testDecoder() {
+		EmbeddedChannel ch = new EmbeddedChannel();
+		UUID id = UUID.randomUUID();
+		TankJoinMsg msg = new TankJoinMsg(5,10, Dir.DOWN,true, Group.GOOD,id);
+		ch.pipeline()
+				.addLast(new TankJoinMsgDecoder());
+		ByteBuf buf = Unpooled.buffer();
+		buf.writeBytes(msg.toBytes());
+
+		ByteBuf dbuf = buf.duplicate();
+		ch.writeInbound(dbuf);
+		ch.flush();
+		TankJoinMsg msgR = (TankJoinMsg) ch.readInbound();
+		Assertions.assertEquals(msg.getX(),msgR.getX());
+		Assertions.assertEquals(msg.getY(),msgR.getY());
+		Assertions.assertEquals(msg.getDir(),msgR.getDir());
+		Assertions.assertEquals(msg.isMoving(),msgR.isMoving());
+		Assertions.assertEquals(msg.getGroup(),msgR.getGroup());
+		Assertions.assertEquals(msg.getId(),msgR.getId());
+	}
+
+}
