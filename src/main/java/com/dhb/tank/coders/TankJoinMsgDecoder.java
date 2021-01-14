2d0c4dd4fe5e9f610084c369bc499779a9ffc7ba
haibo.duan <dhaibo1986@sohu.com>
2021-01-14 16:05:00 +0800

    解决DID NOT READ ANYTHING BUT DECODED A MESSAGE 异常

--- src/main/java/com/dhb/tank/coders/TankJoinMsgDecoder.java
+++ src/main/java/com/dhb/tank/coders/TankJoinMsgDecoder.java
@@ -1,13 +1,16 @@
 package com.dhb.tank.coders;
 
+import com.dhb.tank.comms.Dir;
+import com.dhb.tank.comms.Group;
 import com.dhb.tank.mode.TankJoinMsg;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.ByteToMessageDecoder;
 
 import java.util.List;
+import java.util.UUID;
 
-public class TankJoinDecoder  extends ByteToMessageDecoder{
+public class TankJoinMsgDecoder extends ByteToMessageDecoder{
 
 	@Override
 	protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
@@ -16,7 +19,12 @@ public class TankJoinDecoder  extends ByteToMessageDecoder{
 			return;
 		}
 		TankJoinMsg msg = new TankJoinMsg();
-		msg.parse(in.array());
+		msg.setX(in.readInt());
+		msg.setY(in.readInt());
+		msg.setDir(Dir.values()[in.readInt()]);
+		msg.setMoving(in.readBoolean());
+		msg.setGroup(Group.values()[in.readInt()]);
+		msg.setId(new UUID(in.readLong(),in.readLong()));
 		out.add(msg);
 	}
 }
--- src/main/java/com/dhb/tank/coders/TankJoinMsgEncoder.java
+++ src/main/java/com/dhb/tank/coders/TankJoinMsgEncoder.java
@@ -5,7 +5,7 @@ import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToByteEncoder;
 
-public class TankJoinEncoder extends MessageToByteEncoder<TankJoinMsg> {
+public class TankJoinMsgEncoder extends MessageToByteEncoder<TankJoinMsg> {
 
 	@Override
 	protected void encode(ChannelHandlerContext ctx, TankJoinMsg msg, ByteBuf out) throws Exception {
