6212d2a88ac1bbb9a13a942b994d0d0c67c60cae
haibo.duan <dhaibo1986@sohu.com>
2021-01-15 14:06:28 +0800

    主函数启动，连接到服务端

--- src/main/java/com/dhb/tank/Main.java
+++ src/main/java/com/dhb/tank/Main.java
@@ -1,8 +1,30 @@
 package com.dhb.tank;
 
+import com.dhb.tank.client.Client;
+import com.dhb.tank.comms.Audio;
+import com.dhb.tank.frame.TankFrame;
+
+import java.util.concurrent.TimeUnit;
+
 public class Main {
 
 	public static void main(String[] args) {
+		TankFrame frame = TankFrame.INSTANCE;
+		frame.setVisible(true);
+		new Thread(() -> new Audio("audio/war1.wav").loop()).start();
+		new Thread(() -> {
+			while (true) {
+				try {
+					TimeUnit.MILLISECONDS.sleep(100);
+					frame.repaint();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}).start();
 
+		Client c = new Client();
+		c.connect();
 	}
+
 }
