b6872dd36433bbf33083aa30ef833fb32e2734bc
haibo.duan <dhaibo1986@sohu.com>
2021-01-19 14:41:23 +0800

    重构ClientHandler,将泛型修改为父类Msg

--- src/main/java/com/dhb/tank/client/ClientHandler.java
+++ src/main/java/com/dhb/tank/client/ClientHandler.java
@@ -1,16 +1,18 @@
 package com.dhb.tank.client;
 
 import com.dhb.tank.frame.TankFrame;
+import com.dhb.tank.mode.Msg;
 import com.dhb.tank.mode.Tank;
 import com.dhb.tank.mode.TankJoinMsg;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 
 
-public class ClientHandler extends SimpleChannelInboundHandler<TankJoinMsg> {
+public class ClientHandler extends SimpleChannelInboundHandler<Msg> {
 
 	@Override
-	protected void channelRead0(ChannelHandlerContext ctx, TankJoinMsg msg) throws Exception {
+	protected void channelRead0(ChannelHandlerContext ctx, Msg msg) throws Exception {
+		System.out.println(msg);
 		msg.handle();
 	}
 
