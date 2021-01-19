53095ab07f588a752d0b19983dfc1391e078faf0
haibo.duan <dhaibo1986@sohu.com>
2021-01-19 14:27:54 +0800

    增加TankStartMovingMsgTest的单元测试类，以及修改TankJoinMsgTest在重构过Decoder和Encoder之后的测试代码

--- src/test/java/TankStartMovingMsgTest.java
+++ src/test/java/TankStartMovingMsgTest.java
@@ -4,6 +4,7 @@ import com.dhb.tank.comms.Dir;
 import com.dhb.tank.comms.Group;
 import com.dhb.tank.mode.MsgType;
 import com.dhb.tank.mode.TankJoinMsg;
+import com.dhb.tank.mode.TankStartMovingMsg;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.embedded.EmbeddedChannel;
@@ -12,38 +13,35 @@ import org.junit.jupiter.api.Test;
 
 import java.util.UUID;
 
-public class TankJoinMsgTest {
+public class TankStartMovingMsgTest {
+
 
 	@Test
 	void testEncoder() {
 		EmbeddedChannel ch = new EmbeddedChannel();
 		UUID id = UUID.randomUUID();
-		TankJoinMsg msg = new TankJoinMsg(5,10, Dir.DOWN,true, Group.GOOD,id);
+		TankStartMovingMsg msg = new TankStartMovingMsg(5,10, Dir.DOWN,id);
 		ch.pipeline()
 				.addLast(new TankMsgEncoder());
 		ch.writeOutbound(msg);
 
+
 		ByteBuf buf = (ByteBuf) ch.readOutbound();
 		MsgType msgType = MsgType.values()[buf.readInt()];
-		Assertions.assertEquals(MsgType.TankJoin,msgType);
+		Assertions.assertEquals(MsgType.TankStartMoving,msgType);
 
 		int length = buf.readInt();
-		Assertions.assertEquals(33,length);
+		Assertions.assertEquals(28,length);
 
 		int x = buf.readInt();
 		int y = buf.readInt();
 		int dirOrdinal = buf.readInt();
 		Dir dir = Dir.values()[dirOrdinal];
-		boolean moving = buf.readBoolean();
-		int groupOrdinal = buf.readInt();
-		Group group = Group.values()[groupOrdinal];
 		UUID uuid = new UUID(buf.readLong(),buf.readLong());
 
 		Assertions.assertEquals(5,x);
 		Assertions.assertEquals(10,y);
 		Assertions.assertEquals(Dir.DOWN,dir);
-		Assertions.assertEquals(true,moving);
-		Assertions.assertEquals(Group.GOOD,group);
 		Assertions.assertEquals(id,uuid);
 	}
 
@@ -51,11 +49,11 @@ public class TankJoinMsgTest {
 	void testDecoder() {
 		EmbeddedChannel ch = new EmbeddedChannel();
 		UUID id = UUID.randomUUID();
-		TankJoinMsg msg = new TankJoinMsg(5,10, Dir.DOWN,true, Group.GOOD,id);
+		TankStartMovingMsg msg = new TankStartMovingMsg(5,10, Dir.DOWN,id);
 		ch.pipeline()
 				.addLast(new TankMsgDecoder());
 		ByteBuf buf = Unpooled.buffer();
-		buf.writeInt(MsgType.TankJoin.ordinal());
+		buf.writeInt(MsgType.TankStartMoving.ordinal());
 		byte[] bytes = msg.toBytes();
 		buf.writeInt(bytes.length);
 		buf.writeBytes(bytes);
@@ -63,13 +61,10 @@ public class TankJoinMsgTest {
 		ByteBuf dbuf = buf.duplicate();
 		ch.writeInbound(dbuf);
 		ch.flush();
-		TankJoinMsg msgR = (TankJoinMsg) ch.readInbound();
+		TankStartMovingMsg msgR = (TankStartMovingMsg) ch.readInbound();
 		Assertions.assertEquals(msg.getX(),msgR.getX());
 		Assertions.assertEquals(msg.getY(),msgR.getY());
 		Assertions.assertEquals(msg.getDir(),msgR.getDir());
-		Assertions.assertEquals(msg.isMoving(),msgR.isMoving());
-		Assertions.assertEquals(msg.getGroup(),msgR.getGroup());
 		Assertions.assertEquals(msg.getId(),msgR.getId());
 	}
-
 }
