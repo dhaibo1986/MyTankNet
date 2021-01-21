84c27dda16c652d862d83d49ce32a8a6086f7ab6
haibo.duan <dhaibo1986@sohu.com>
2021-01-21 14:14:03 +0800

    增加坦克改变方向的事件

--- src/main/java/com/dhb/tank/frame/MyKeyListener.java
+++ src/main/java/com/dhb/tank/frame/MyKeyListener.java
@@ -2,9 +2,8 @@ package com.dhb.tank.frame;
 
 import com.dhb.tank.client.Client;
 import com.dhb.tank.comms.Dir;
-import com.dhb.tank.mode.Bullet;
-import com.dhb.tank.mode.BulletNewMsg;
 import com.dhb.tank.mode.Tank;
+import com.dhb.tank.mode.TankDirChangedMsg;
 import com.dhb.tank.mode.TankStartMovingMsg;
 import com.dhb.tank.mode.TankStopMsg;
 
@@ -24,15 +23,19 @@ public class MyKeyListener extends KeyAdapter {
 		switch (key) {
 			case KeyEvent.VK_LEFT:
 				BL = true;
+				setMainTankDir();
 				break;
 			case KeyEvent.VK_UP:
 				BU = true;
+				setMainTankDir();
 				break;
 			case KeyEvent.VK_DOWN:
 				BD = true;
+				setMainTankDir();
 				break;
 			case KeyEvent.VK_RIGHT:
 				BR = true;
+				setMainTankDir();
 				break;
 //			case KeyEvent.VK_S:
 //				GameModel.getInstance().save();
@@ -41,28 +44,32 @@ public class MyKeyListener extends KeyAdapter {
 			default:
 				break;
 		}
-		setMainTankDir();
+
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
 		Tank myTank = TankFrame.INSTANCE.getMainTank();
-		if(!myTank.isLiving()){
+		if (!myTank.isLiving()) {
 			return;
 		}
 		int key = e.getExtendedKeyCode();
 		switch (key) {
 			case KeyEvent.VK_LEFT:
 				BL = false;
+				setMainTankDir();
 				break;
 			case KeyEvent.VK_UP:
 				BU = false;
+				setMainTankDir();
 				break;
 			case KeyEvent.VK_DOWN:
 				BD = false;
+				setMainTankDir();
 				break;
 			case KeyEvent.VK_RIGHT:
 				BR = false;
+				setMainTankDir();
 				break;
 
 			case KeyEvent.VK_CONTROL:
@@ -71,14 +78,16 @@ public class MyKeyListener extends KeyAdapter {
 			default:
 				break;
 		}
-		setMainTankDir();
 	}
 
 	private void setMainTankDir() {
 		Tank myTank = TankFrame.INSTANCE.getMainTank();
-		if(!myTank.isLiving()){
+		Dir dir = myTank.getDir();
+
+		if (!myTank.isLiving()) {
 			return;
 		}
+
 		if (!BL && !BR && !BU && !BD) {
 			myTank.setMoving(false);
 			Client.INSTANCE.send(new TankStopMsg(myTank));
@@ -95,10 +104,15 @@ public class MyKeyListener extends KeyAdapter {
 			if (BD) {
 				myTank.setDir(Dir.DOWN);
 			}
-			if(!myTank.isMoving()) {
+			//发出坦克移动的消息
+			if (!myTank.isMoving()) {
 				Client.INSTANCE.send(new TankStartMovingMsg(myTank));
 			}
 			myTank.setMoving(true);
+			//发出坦克改变方向的消息
+			if (dir != myTank.getDir()) {
+				Client.INSTANCE.send(new TankDirChangedMsg(myTank));
+			}
 		}
 	}
 }
