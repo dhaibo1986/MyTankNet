1bc24b641b7b2114a5ff9c8a900f267bd1068fd4
haibo.duan <dhaibo1986@sohu.com>
2021-01-27 14:25:04 +0800

    增加坦克碰撞的责任链处理器

---
+++ src/main/java/com/dhb/tank/cor/ColiderChain.java
@@ -0,0 +1,38 @@
+package com.dhb.tank.cor;
+
+import com.dhb.tank.comms.ProrertyMgr;
+import com.dhb.tank.mode.GameObject;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class ColiderChain implements Colider{
+
+	public ColiderChain() {
+		List<String> list  = ProrertyMgr.getList("coliders");
+		for(String clazz : list) {
+			try {
+				this.coliders.add((Colider) Class.forName(clazz).newInstance());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	private List<Colider> coliders = new LinkedList<>();
+
+	public void add(Colider colider) {
+		this.coliders.add(colider);
+	}
+
+	@Override
+	public boolean colide(GameObject o1, GameObject o2) {
+		for(Colider colider : coliders) {
+			if(!colider.colide(o1,o2)){
+				return true;
+			}
+		}
+		return true;
+	}
+}
