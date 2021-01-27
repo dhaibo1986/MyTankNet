674e8f67ca0dec26fa4285a82d78573943412463
haibo.duan <dhaibo1986@sohu.com>
2021-01-27 15:33:05 +0800

    添加碰撞效果的责任链

---
+++ src/main/java/com/dhb/tank/cor/BulletTankColider.java
@@ -0,0 +1,31 @@
+package com.dhb.tank.cor;
+
+import com.dhb.tank.mode.Bullet;
+import com.dhb.tank.mode.Explode;
+import com.dhb.tank.mode.GameObject;
+import com.dhb.tank.mode.Tank;
+
+public class BulletTankColider implements Colider {
+
+	@Override
+	public boolean colide(GameObject o1, GameObject o2) {
+		if(o1 instanceof Bullet && o2 instanceof Tank) {
+			Bullet bullet = (Bullet) o1;
+			Tank tank = (Tank) o2;
+			if (bullet.getGroup() == tank.getGroup()) {
+				return true;
+			}
+			if (bullet.getRect().intersects(tank.getRect())) {
+				tank.die();
+				bullet.die();
+				int eX = tank.getX() + Tank.WIDTH / 2 - Explode.WIDTH / 2;
+				int eY = tank.getY() + Tank.HEIGHT / 2 - Explode.HEIGHT / 2;
+				new Explode(eX, eY, true);
+			}
+			return false;
+		} else if(o2 instanceof Bullet && o1 instanceof Tank) {
+			colide(o2,o1);
+		}
+		return true;
+	}
+}
---
+++ src/main/java/com/dhb/tank/cor/BulletWallColider.java
@@ -0,0 +1,26 @@
+package com.dhb.tank.cor;
+
+import com.dhb.tank.mode.Bullet;
+import com.dhb.tank.mode.Explode;
+import com.dhb.tank.mode.GameObject;
+import com.dhb.tank.mode.Wall;
+
+public class BulletWallColider implements Colider {
+
+	@Override
+	public boolean colide(GameObject o1, GameObject o2) {
+		if(o1 instanceof Bullet && o2 instanceof Wall) {
+			Bullet bullet = (Bullet) o1;
+			Wall wall = (Wall) o2;
+			if (bullet.getRect().intersects(wall.getRect())) {
+				bullet.die();
+				int eX = bullet.getX() + Bullet.WIDTH / 2 - Bullet.WIDTH / 2;
+				int eY = bullet.getY() + Bullet.HEIGHT / 2 - Bullet.HEIGHT / 2;
+				new Explode(eX, eY, true);
+			}
+		} else if(o2 instanceof Bullet && o1 instanceof Wall) {
+			colide(o2,o1);
+		}
+		return true;
+	}
+}
